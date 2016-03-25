47cfd527f57a11eb963ea11a05c4de5bd490a3fd
Zarkades <zarkades@gmail.com>
2016-03-25 14:18:56 -0700

    Checkpoint rails-testing

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+# #1 Describe the subject of the spec, WelcomeController
+RSpec.describe WelcomeController, type: :controller do
+  describe "GET index" do
+    it "renders the index template" do
+# #2 Use get to call the index method of WelcomeController
+      get :index
+# #3 Expect the controller's response to render the index template
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET about" do
+    it "renders the about template" do
+      get :about
+      expect(response).to render_template("about")
+    end
+  end
+end
